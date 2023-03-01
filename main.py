93a366c14fe8484edd611875e026c78d5be424ab
incorrect-horse <vincenso_h@yahoo.com>
2023-03-01 18:17:18 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,58 @@
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, \
+    QLineEdit, QPushButton, QComboBox, QMainWindow, QTableWidget, \
+    QTableWidgetItem
+from PyQt6.QtGui import QAction
+import sys
+import sqlite3
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Student Management System")
+
+        file_menu_item = self.menuBar().addMenu("&File")
+        help_menu_item = self.menuBar().addMenu("&Help")
+
+        add_student_action = QAction("Add Student", self)
+        file_menu_item.addAction(add_student_action)
+
+        about_action = QAction("About", self)
+        help_menu_item.addAction(about_action)
+
+        self.table = QTableWidget()
+        self.table.setColumnCount(4)
+        self.table.setHorizontalHeaderLabels(("ID", "Name", "Course", "Mobile"))
+        self.table.verticalHeader().setVisible(False)
+        self.setCentralWidget(self.table)
+
+    def load_data(self):
+        connection = sqlite3.connect("database.db")
+        result = connection.execute("SELECT * FROM students")
+        self.table.setRowCount(0)
+        for row_number, row_data, in enumerate(result):
+            self.table.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                print(row_data)
+                self.table.setItem(row_number, column_number,
+                                   QTableWidgetItem(str(data)))
+        connection.close()
+
+app = QApplication(sys.argv)
+student_mgmt_sys = MainWindow()
+student_mgmt_sys.show()
+student_mgmt_sys.load_data()
+sys.exit(app.exec())
+
+"""
+project requirements
+- view students
+    ID
+    name
+    course
+    phone #
+insert new records
+update records
+delete records
+search records
+sort records
+"""
